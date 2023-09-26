019871e4b52b142bcddac85390e543baf7fcf7f1
Akamig <akamig@posteo.net>
2023-09-26 15:26:49 +0900

    I guess it's pretty much sanitized?

--- src/modules/main/src/mainWindow.ts
+++ src/modules/main/src/mainWindow.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import { join, resolve } from "node:path";
+import logoImage from "../../buildAssets/logo.png";
 
 async function createWindow() {
   const browserWindow = new BrowserWindow({
@@ -11,6 +12,10 @@ async function createWindow() {
       webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
       preload: join(app.getAppPath(), "packages/preload/dist/index.cjs"),
     },
+    frame: false,
+    autoHideMenuBar: true,
+    titleBarStyle: process.platform === "darwin" ? "hidden" : undefined,
+    icon: join(app.getAppPath(), logoImage),
   });
 
   /**
